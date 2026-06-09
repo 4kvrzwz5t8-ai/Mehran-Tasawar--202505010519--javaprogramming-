cd3cd82bfb6b6071418001454a5973ae5dd6c5e0
4kvrzwz5t8-ai <4kvrzwz5t8@privaterelay.appleid.com>
2026-06-09 08:21:40 +0000

    Mehran

---
+++ main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // coding starts here;
+
+        Student s1 = new Student("Ali",20, "Computer Science");
+
+        s1.displayInfo();
+
+        s1.setAge(21);
+        s1.displayInfo();
+    }
+}
